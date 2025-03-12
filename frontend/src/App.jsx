6c85b422126f4660c950f2e1bd5a4ef95118d69f
Fabienholert <fabienholert@gmail.com>
2025-03-12 17:44:13 +0100

    Ajout de la structure de base pour l'application React avec Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home/home.jsx";
+import Layout from "./layout.jsx";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profil" element={<Profil />} />
+            <Route path="/tableau" element={<Tableau />} />
+            <Route path="/carte" element={<Carte />} />
+            <Route path="/mail" element={<Mail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
