28428d979da0dd8dcab69968b134c81e78382cfc
Fabienholert <fabienholert@gmail.com>
2025-03-12 21:47:24 +0100

    Ajout d'un formulaire d'inscription sur la page d'accueil

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,3 +1,26 @@
 export default function Home() {
-  return <div>bonjour</div>;
+  return (
+    <div className="Home__container">
+      <form>
+        <label for="email"> Email </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Votre email"
+          required
+        />
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="email"
+          name="password"
+          placeholder="votre mot de passe"
+          required
+        />
+        <input type="submit" value="Envoyer" />
+        <input type="submit" value="S inscrire" />
+      </form>
+    </div>
+  );
 }
